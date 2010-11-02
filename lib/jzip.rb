01a8c68da85af4a6a8eb2ca52c996615b6109fdb
Mark Mulder <markmulder@gmail.com>
2010-11-02 16:13:53 +0100

    Explicitly require minifier.
    Still needs some researching as to why this is necessary in Rails 3 and
    not in Rails 2.

--- lib/jzip.rb
+++ lib/jzip.rb
@@ -7,6 +7,7 @@ require "jzip/core"
 require "jzip/actionpack"
 
 require "jzip/engine/support/notifier"
+require "jzip/engine/support/minifier"
 require "jzip/engine"
 require "jzip/engine/template"
 require "jzip/engine/requirement"
