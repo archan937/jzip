d403da0ca06b11abb900b6bcae36ebb016adf509
Paul Engel <paul.engel@holder.nl>
2012-04-16 23:49:34 +0200

    Cleaned up template.rb

--- lib/jzip/engine/template.rb
+++ lib/jzip/engine/template.rb
@@ -5,7 +5,7 @@ module Jzip
 
       @@instances = {}
       attr_reader :target_file
-      attr_accessor :template, :source, :target
+      attr_accessor :file, :source, :target
 
       def self.clear_instances
         @@instances.clear
@@ -15,15 +15,15 @@ module Jzip
         @@instances[args.first] ||= self.new(*args)
       end
 
-      def initialize(template, source, target)
-        self.template = template
+      def initialize(file, source, target)
+        self.file = file
         self.source = source
         self.target = target
 
-        @file_name = File.basename(self.template, ".jz") + ".js"
+        @file_name = File.basename(self.file, ".jz") + ".js"
         @target_dir = begin
           dir = partial? ? self.target.gsub(Engine.options[:root_dir], Engine.tmp_dir) : self.target
-          File.dirname File.join dir, self.template.gsub(self.source, "")
+          File.dirname File.join dir, self.file.gsub(self.source, "")
         end
         @target_file = File.join @target_dir, @file_name
 
@@ -31,42 +31,21 @@ module Jzip
       end
 
       def parse
-        if requires_parsing?
-          @code = @segments.collect do |segment|
-                    segment.is_a?(Requirement) ?
-                      segment.code :
-                      segment.join("")
-                  end.
-                  join("\n\n").
-                  gsub(/^\s*$\n{2,}/, "\n").
-                  strip.insert(0, "\n").insert(-1, "\n")
-
-          write_file
-        end
-        @code ||= File.read(@target_file)
+        @code ||= begin
+                    write_file if requires_parsing?
+                    File.read @target_file
+                  end
       end
 
       def code
         parse
       end
 
-      def outdated?
-        missing? || (File.mtime(@target_file) < File.mtime(self.template))
-      end
-
-      def partial?
-        !!@file_name.match(REG_EXPS[:partial])
-      end
-
-      def minify?
-        Jzip::Engine::options[:minify]
-      end
-
     private
 
       def scan_template
         @requirements = []
-        @segments     = File.readlines(self.template).inject([]) do |segments, line|
+        @segments     = File.readlines(self.file).inject([]) do |segments, line|
                           if line.jzip_require_statement?
                             derive_required_source(line.required_jzip_source).each do |file|
                               @requirements << Requirement.new(file, self.source, self.target)
@@ -86,7 +65,7 @@ module Jzip
 
         source_dirname = required_source.match(REG_EXPS[:output_dir]) ?
                            Engine.options[:output_dir] :
-                           File.dirname(self.template)
+                           File.dirname(self.file)
         sources        = begin
                            if PREDEFINED_SETS.include?(basename)
                              PREDEFINED_SETS[basename].collect{|x| File.join(dirname, x)}
@@ -110,12 +89,21 @@ module Jzip
       def write_file
         notify "  create #{@target_file.gsub(/#{File.expand_path(".")}\//, "")}"
 
+        code = @segments.collect do |segment|
+                 segment.is_a?(Requirement) ?
+                   segment.code :
+                   segment.join("")
+               end.
+               join("\n\n").
+               gsub(/^\s*$\n{2,}/, "\n").
+               strip.insert(0, "\n").insert(-1, "\n")
+
         FileUtils.mkdir_p @target_dir
         File.open(@target_file, "w") do |f|
-          f.write @code
+          f.write code
         end
 
-        Support::Minifier.parse @target_file if minify?
+        Support::Minifier.parse @target_file if Jzip::Engine::options[:minify]
       end
 
       def requires_parsing?
@@ -127,12 +115,16 @@ module Jzip
         @requirements.any?{|x| x.newer? modification_time}
       end
 
-      def missing?
-        !target_file?
+      def outdated?
+        missing? || (File.mtime(@target_file) < File.mtime(self.file))
       end
 
-      def target_file?
-        File.exists? @target_file
+      def missing?
+        !File.exists?(@target_file)
+      end
+
+      def partial?
+        !!@file_name.match(REG_EXPS[:partial])
       end
 
     end
